aa3cf2a149bbe2f075c625d7fbd03778c731e03d
tororo.in <aj.anthonydsouza@gmail.com>
2024-12-10 16:26:40 +0100

    trying resolving package issue

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import find_packages, setup
 
 setup(
     name = "KittyLM",
-    version = "0.0.2.dev0",
-    packages = find_packages("KittyLM")
+    version = "0.0.3.dev0",
+    packages = find_packages(include = ["KittyLM", "KittyLM."])
 )
